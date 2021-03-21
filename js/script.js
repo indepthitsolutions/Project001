5a8c25d4467d2dd7f8aa26eb6d41b515bd85dcf7
Mihir <mp06121@gmail.com>
2021-03-21 20:13:58 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,258 @@
+(function($) { "use strict";
+
+	
+	//Portfolio Top Sections Fullscreen	
+				
+	$(function(){"use strict";
+		$('.portfolio-top').css({'height':($(window).height())+'px'});
+		$(window).resize(function(){
+		$('.portfolio-top').css({'height':($(window).height())+'px'});
+		});
+	});
+	
+	
+	/* Scroll Too */
+	
+	$(document).ready(function(){"use strict";
+		$(".scroll").click(function(event){
+
+			event.preventDefault();
+
+			var full_url = this.href;
+			var parts = full_url.split("#");
+			var trgt = parts[1];
+			var target_offset = $("#"+trgt).offset();
+			var target_top = target_offset.top;
+
+			$('html, body').animate({scrollTop:target_top}, 1000);
+		});
+	});
+	
+	
+	//Tooltip
+	
+	$(".tipped").tipper();
+ 
+
+	/* Portfolio Sorting */
+
+	jQuery(document).ready(function($){
+		(function ($) { 
+		
+		
+			var container = $('#projects-grid');
+			
+			
+			function getNumbColumns() { 
+				var winWidth = $(window).width(), 
+					columnNumb = 1;
+				
+				
+				if (winWidth > 1500) {
+					columnNumb = 4;
+				} else if (winWidth > 1200) {
+					columnNumb = 3;
+				} else if (winWidth > 900) {
+					columnNumb = 2;
+				} else if (winWidth > 600) {
+					columnNumb = 2;
+				} else if (winWidth > 300) {
+					columnNumb = 1;
+				}
+				
+				return columnNumb;
+			}
+			
+			
+			function setColumnWidth() { 
+				var winWidth = $(window).width(), 
+					columnNumb = getNumbColumns(), 
+					postWidth = Math.floor(winWidth / columnNumb);
+
+			}
+			
+			$('#portfolio-filter #filter a').click(function () { 
+				var selector = $(this).attr('data-filter');
+				
+				$(this).parent().parent().find('a').removeClass('current');
+				$(this).addClass('current');
+				
+				container.isotope( { 
+					filter : selector 
+				});
+				
+				setTimeout(function () { 
+					reArrangeProjects();
+				}, 300);
+				
+				
+				return false;
+			});
+			
+			function reArrangeProjects() { 
+				setColumnWidth();
+				container.isotope('reLayout');
+			}
+			
+			
+			container.imagesLoaded(function () { 
+				setColumnWidth();
+				
+				
+				container.isotope( { 
+					itemSelector : '.portfolio-box-1', 
+					layoutMode : 'masonry', 
+					resizable : false 
+				} );
+			} );
+			
+			
+		
+			
+		
+			$(window).on('debouncedresize', function () { 
+				reArrangeProjects();
+				
+			} );
+			
+		
+		} )(jQuery);
+	} );
+
+
+
+
+
+	/* DebouncedResize Function */
+		(function ($) { 
+			var $event = $.event, 
+				$special, 
+				resizeTimeout;
+			
+			
+			$special = $event.special.debouncedresize = { 
+				setup : function () { 
+					$(this).on('resize', $special.handler);
+				}, 
+				teardown : function () { 
+					$(this).off('resize', $special.handler);
+				}, 
+				handler : function (event, execAsap) { 
+					var context = this, 
+						args = arguments, 
+						dispatch = function () { 
+							event.type = 'debouncedresize';
+							
+							$event.dispatch.apply(context, args);
+						};
+					
+					
+					if (resizeTimeout) {
+						clearTimeout(resizeTimeout);
+					}
+					
+					
+					execAsap ? dispatch() : resizeTimeout = setTimeout(dispatch, $special.threshold);
+				}, 
+				threshold : 150 
+			};
+		} )(jQuery);
+
+		
+ 
+ 
+  })(jQuery); 
+
+
+  /*!
+ * Start Bootstrap - Grayscale v6.0.2 (https://startbootstrap.com/themes/grayscale)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+ */
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ?
+                target :
+                $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate({
+                        scrollTop: target.offset().top - 70,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 100,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery);
+
+AOS.init({
+    duration: 1500,
+});
+
+$(document).ready(function () {
+    $(this).scrollTop(0);
+});
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
+});
+
+// End of use strict
